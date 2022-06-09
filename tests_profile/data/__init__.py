8e852ff6c5543b5cc6c5875b4e715e73c2c6f30c
ionite34 <dev@ionite.io>
2022-06-08 23:46:37 -0400

    Added data module enum helper

--- tests_profile/data/__init__.py
+++ tests_profile/data/__init__.py
@@ -21,3 +21,28 @@ def enum(num_files: int) -> list[(str,  str)]:
             break
         out_f = file.replace('.wav', '_out.wav')
         yield file, out_f
+
+
+def enum_batch(per_batch: int, batches: int) -> list[list[(str, str)]]:
+    """
+    Enumerates the files in the input directory, splitting into batches
+    """
+    # Do a glob to get all wav files
+    files = glob.glob(f"{DATA_PATH}/*.wav")
+
+    total_files = per_batch * batches
+
+    if len(files) < total_files:
+        raise ValueError(f"Not enough files in {DATA_PATH},"
+                         f" expected {per_batch}, found {len(files)}")
+
+    result = []
+    for i in range(batches):
+        batch = []
+        for j in range(per_batch):
+            file = files[i * per_batch + j]
+            out_f = file.replace('.wav', '_out.wav')
+            batch.append((file, out_f))
+        result.append(batch)
+
+    return result
