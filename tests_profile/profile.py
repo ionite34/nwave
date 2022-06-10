c0f237bd8bf496e33aa5edcc4c6207600b482c53
ionite34 <dev@ionite.io>
2022-06-09 20:05:20 -0400

    Removed loaded testing

--- tests_profile/profile.py
+++ tests_profile/profile.py
@@ -1,14 +1,16 @@
 # Performance Profiling Tests
 from __future__ import annotations
-from joblib import Parallel, delayed, parallel_backend
+
 import os
 import time
-from tqdm.auto import tqdm
-from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 from concurrent import futures
-from nwave import audio as pa
-# from tests_profile.resources import profile_audio as pa
+from concurrent.futures import ThreadPoolExecutor
+
+from joblib import Parallel, delayed
+from tqdm.auto import tqdm
+
 from nwave import Task, Config
+from nwave import audio
 from tests_profile import data
 
 
@@ -65,7 +67,7 @@ def profile_audio(n_files: int, cfg: Config, batch_num: int = 1):
         for batch in tqdm(total):
             for in_f, out_f in batch:
                 task = Task(in_f, out_f, cfg, overwrite=True)
-                pa.process_file(task)
+                audio.process_file(task)
 
 
 def profile_audio_parallel(n_files: int, cfg: Config, threads: int, batch_num: int = 1):
@@ -73,7 +75,7 @@ def profile_audio_parallel(n_files: int, cfg: Config, threads: int, batch_num: i
     with Time(verbose=True):
         with Parallel(n_jobs=threads, backend='threading') as thread_pool:
             for batch in total:
-                thread_pool(delayed(pa.process_file)(
+                thread_pool(delayed(audio.process_file)(
                     Task(in_f, out_f, cfg, overwrite=True)
                 ) for in_f, out_f in batch)
 
@@ -84,7 +86,7 @@ def profile_audio_threadpool(n_files: int, cfg: Config, threads: int, batch_num:
         with ThreadPoolExecutor(max_workers=threads) as executor:
             for batch in total:
                 tasks = {executor.submit(
-                    pa.process_file, Task(in_f, out_f, cfg, overwrite=True)
+                    audio.process_file, Task(in_f, out_f, cfg, overwrite=True)
                 ): (in_f, out_f) for (in_f, out_f) in batch}
             # Wait for all tasks to complete
             for future in tqdm(futures.as_completed(tasks)):
@@ -101,7 +103,7 @@ def profile_audio_thread_map(n_files: int, cfg: Config, threads: int, batch_num:
         with ThreadPoolExecutor(max_workers=threads) as thread_pool:
             for batch in total:
                 jobs = [Task(in_f, out_f, cfg, overwrite=True) for in_f, out_f in batch]
-                results = thread_pool.map(pa.process_file, jobs)
+                results = thread_pool.map(audio.process_file, jobs)
 
 
 def clean_up():
@@ -154,29 +156,6 @@ def main():
     print('-' * 3)
     clean_up()
     time.sleep(1)
-
-
-def load_main():
-    from cpu_load_generator import load_all_cores
-    from multiprocessing import Process
-    import psutil
-
-    print('-' * 5)
-    print('Starting Load tests...')
-    print('Loading 16 logical cores')
-    p = Process(target=load_all_cores, args=(30, 0.99))
-    p.start()
-    time.sleep(3)  # Wait for load
-    cur_load = psutil.cpu_percent(1)
-    print(f'Load reached, current load: {cur_load:.4f} %')
-
-    print('-' * 5)
-    print('(With load) [Single]')
-    profile_audio(n_files, batch_num=runs)
-    print('-' * 5)
-    print(f'(With load) [Parallel] {threads} threads')
-    profile_audio_parallel(n_files, threads, batch_num=runs)
-    p.terminate()
 
 
 if __name__ == '__main__':
